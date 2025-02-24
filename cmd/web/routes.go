d3cdebc34caea06cc2b17d9d11812276fb3e9ecc
maria.tellez <maria.tellez@sezzle.com>
2025-02-23 22:13:06 -0500

    Adding middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/mariaptv/WebAppGo/pkg/config"
 	"github.com/mariaptv/WebAppGo/pkg/handlers"
 )
-func route (app *config.AppConfig) http.Handler{
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+func route(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
